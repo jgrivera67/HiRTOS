6391d2541f0daebee1d14fb36298866f17422420
J. German Rivera <jgrivera67@gmail.com>
2021-10-15 09:06:22 -0500

    Added HiRTOS interface
    Signed-off-by: J. German Rivera <jgrivera67@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # HiRTOS
+
+This repository hosts HiRTOS, a high-integrity RTOS written in SPARK Ada.
+
+## Motivation
+
+An RTOS is a safety critical component of any bare-metal embedded software system.
+Yet, most RTOSes are written in C which is an unsafe language. It would be safer
+to use an RTOS written in a safer language, such as Ada or even better SPARK Ada.
+However, integrating Ada code components in bare-metal embedded firmware written
+in other languages, typically C, is not easy in a portable manner, as the available
+baremetal GNAT cross-compiler from AdaCore requires the availability of an Ada Runtime
+for the target CPU architecture and the FSF GNAT compiler is not available as a
+baremetal cross-compiler, unless you port it yourself which can be very challenging
+and time consuming.
+	.
+
+## Formal Specification
+
+A Formal specification of HiRTOS can be found [here](doc/HiRTOS.pdf).
+
---
+++ src/hirtos_parameters.ads
@@ -0,0 +1,63 @@
+--
+--  Copyright (c) 2021, German Rivera
+--  All rights reserved.
+--
+--  Redistribution and use in source and binary forms, with or without
+--  modification, are permitted provided that the following conditions are met:
+--
+--  * Redistributions of source code must retain the above copyright notice,
+--    this list of conditions and the following disclaimer.
+--
+--  * Redistributions in binary form must reproduce the above copyright notice,
+--    this list of conditions and the following disclaimer in the documentation
+--    and/or other materials provided with the distribution.
+--
+--  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+--  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+--  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+--  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+--  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+--  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+--  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+--  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+--  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+--  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+--  POSSIBILITY OF SUCH DAMAGE.
+--
+
+--
+--  @summary HiRTOS compile-time configuration parameters
+--
+package HiRTOS_Parameters with SPARK_Mode => On is
+   --
+   --  Maximum number of threads
+   --
+   Max_Num_Threads : constant := 32;
+
+   --
+   --  Maximum number of condition variables (not counting the condvar embedded
+   --  in each thread)
+   --
+   Max_Num_Condvars : constant := 32;
+
+   --
+   --  Maximum number of mutexes
+   --
+   Max_Num_Mutexes : constant := 32;
+
+   --
+   --  Maximum number of timers
+   --
+   Max_Num_Timers : constant := Max_Num_Threads + 32;
+
+   --
+   --  Number of thread priorities
+   --
+   Num_Thread_Priorities : constant := 16;
+
+   --
+   --  Thread stack minimum size in bytes
+   --
+   Thread_Stack_Min_Size : constant := 1024;
+
+end HiRTOS_Parameters;
