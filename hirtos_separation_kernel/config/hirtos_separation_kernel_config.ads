03d56c4dce3d8cf33b6f6636e0366b6c392cf3df
J. German Rivera <jgrivera67@gmail.com>
2023-12-19 00:49:01 -0500

    Fix to remove dependency on portable_minimal_ada_rts crate
    HiRTOS does not build with the published portable_minimal_ada_rts
    crate. Some symbols were removed from the published portable_minimal_ada_rts
    crate to be able to pass the Windows builds merge prerequisites of the
    alire-index repo. However, it turns out the removed symbols are needed
    to be able to compile HiRTOS.
    
    Signed-off-by: J. German Rivera <jgrivera67@gmail.com>

--- hirtos_separation_kernel/config/hirtos_separation_kernel_config.ads
+++ hirtos_separation_kernel/config/hirtos_separation_kernel_config.ads
@@ -5,7 +5,7 @@ pragma Style_Checks (Off);
 package Hirtos_Separation_Kernel_Config is
    pragma Pure;
 
-   Crate_Version : constant String := "0.0.0";
+   Crate_Version : constant String := "1.0.0";
    Crate_Name : constant String := "hirtos_separation_kernel";
 
    Alire_Host_OS : constant String := "linux";
